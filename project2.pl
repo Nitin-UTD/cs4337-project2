c7d936a205980290669201ef62b893747a49c397
Nitin Venkat Thota <nvt230000@utdallas.edu>
2026-05-10 17:52:56 -0500

    Added workstation collection helper

--- project2.pl
+++ project2.pl
@@ -2,3 +2,6 @@ plan(_) :- fail.
 
 all_employees(Employees) :-
     findall(Employee, employee(Employee), Employees).
+
+all_workstations(Workstations) :-
+    findall(workstation(Station, Min, Max), workstation(Station, Min, Max), Workstations).
